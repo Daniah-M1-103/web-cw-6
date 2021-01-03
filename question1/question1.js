38cfdd7de39f434350bc7e4fe9fd3c482373f618
Dania-M1-103 <191406@student.kcst.edu.kw>
2021-01-04 02:42:26 +0300

    done

--- question1/question1.js
+++ question1/question1.js
@@ -1,1 +1,21 @@
 // 👇🏻 قم بحل السؤال الأول هنا
+
+
+
+function sayHello(){
+    console.log(`Hey, whats your name`)
+}
+
+function sayHelloPlus(name) {
+    if (name){
+        console.log;(`hello ${name}`)
+    }
+    else{
+        console.log(`hello`)
+    }
+}
+
+
+
+sayHello();
+sayHelloPlus();
